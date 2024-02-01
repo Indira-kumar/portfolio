c2d899db17b5c817e1a61ce9bf3cbfef47a3b745
Indira-kumar <indirakumarak@gmail.com>
2024-02-02 03:23:02 +0530

    <update> : profile pic

--- src/Components/About/Introduction.jsx
+++ src/Components/About/Introduction.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Introduction.css";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { ThemeContext } from "../../Context/theme";
-import profilePic from "../../assets/profile-picture-Indira-kumar.jpeg";
+import profilePic from "../../assets/Indira-kumar-dp.jpeg";
 
 
 export const Introduction = () => {
